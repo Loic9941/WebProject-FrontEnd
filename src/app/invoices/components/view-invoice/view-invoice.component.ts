494e9eee7004a9fd6ef85e5e17362d6ebc90edd8
Loïc Marin <marin.loic@outlook.com>
2025-04-12 20:57:23 +0200

    Add view invoice

--- src/app/invoices/components/view-invoice/view-invoice.component.ts
+++ src/app/invoices/components/view-invoice/view-invoice.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { InvoiceService } from '../../services/invoice.service';
+import { CommonModule } from '@angular/common';
+import { Invoice } from '../../interfaces/invoice.interface';
 
 @Component({
   selector: 'app-view-invoice',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './view-invoice.component.html',
   styleUrl: './view-invoice.component.css'
 })
 export class ViewInvoiceComponent {
+  invoiceId!: number;
+  invoice: any = {
+    id: 0,
+    invoiceItems: [],
+  };
+  totalPrice!: number;
 
+  constructor(
+    private invoiceService: InvoiceService,
+    private route: ActivatedRoute,
+    
+  ) { }
+
+  ngOnInit(): void {
+    this.invoiceId = +this.route.snapshot.paramMap.get('id')!;
+    
+    this.invoiceService.getInvoiceById(this.invoiceId).subscribe((data) => {
+      this.invoice = data;
+      this.totalPrice = 0;
+      for (let i = 0; i < this.invoice.invoiceItems.length; i++) {
+        this.totalPrice += this.invoice.invoiceItems[i].quantity * this.invoice.invoiceItems[i].unitPrice;
+      }
+    });
+  }
+
+  getInvoiceStatus(invoice: Invoice) {
+    return this.invoiceService.getInvoiceStatus(invoice);
+  }
+
+  rateArticle(invoiceItem: any) {
+    // Logic to rate the article
+    console.log('Rate article:', invoiceItem);
+  }
 }
---
+++ src/app/products/components/rate-product/rate-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rate-product',
+  imports: [],
+  templateUrl: './rate-product.component.html',
+  styleUrl: './rate-product.component.css'
+})
+export class RateProductComponent {
+
+}
